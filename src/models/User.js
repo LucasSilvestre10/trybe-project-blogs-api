47f519d8124b7779bb01773eb2ae614a31d5c898
Lucas Silva Silvestre <lucas_silvestre.10@hotmail.com>
2023-04-11 17:55:09 -0300

    requisito 2 feito (criado model User)

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
+  return User;
+}
