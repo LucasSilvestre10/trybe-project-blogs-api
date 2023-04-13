3e2682654674239295988f34e9604bc0e4856b2c
Lucas Silva Silvestre <lucas_silvestre.10@hotmail.com>
2023-04-13 01:40:30 -0300

    requisitos 10 e 11 feitos (adicionado models para BlogPost e PostCategory)

--- src/models/User.js
+++ src/models/User.js
@@ -29,6 +29,11 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
     timestamps: false
   });
+  
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
  
 
   return User;
